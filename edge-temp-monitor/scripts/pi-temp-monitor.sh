c1bbe42167cb2319861b36794e85239c6bc41cce
darensalter71 <daren.salter@icloud.com>
2026-04-28 11:47:17 +0100

    Release version 1.0

--- edge-temp-monitor/scripts/pi-temp-monitor.sh
+++ edge-temp-monitor/scripts/pi-temp-monitor.sh
@@ -1,22 +1,38 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# === Paths ===
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"  # === Root Path ===
 LOGFILE="$SCRIPT_DIR/../data/pi-temp-$(date +%Y-%m).csv"
 
-THRESHOLD=60 # Default threshold temperature (60c)
-FAN="OFF"  # Default fan settng : OFF.
 
-PI_ID=$(cat /proc/cpuinfo | grep Serial | awk '{print $3}')
+# === Load Config File ===
+APP_ENV=${APP_ENV:-prod} # dev | prod
+CONFIG_FILE="$SCRIPT_DIR/../config/.env.$APP_ENV"
 
-TOP_PROCESS=$(ps -eo comm,%cpu --sort=-%cpu | awk 'NR==2 {print $1 " (" $2 "%)"}')
+if [ -f "$CONFIG_FILE" ]; then
+   source "$CONFIG_FILE"
+#   echo "$CONFIG_FILE : Config File Found!"
+else
+#   echo "$CONFIG_FILE : Config File Not Found!"
+   exit 1
+fi
 
-TIMESTAMP="$(date '+%Y-%m-%d %H:%M:%S')"
+#=== Removed to .env file
+#THRESHOLD=60 # Default threshold temperature (60c)
+#FAN="OFF"  # Default fan settng : OFF.
+#PI_ID=$(cat /proc/cpuinfo | grep Serial | awk '{print $3}')
+#TOP_PROCESS=$(ps -eo comm,%cpu --sort=-%cpu | awk 'NR==2 {print $1 " (" $2 "%)"}')
+#TIMESTAMP="$(date '+%Y-%m-%d %H:%M:%S')"
+#=======
 
 #echo "Writing to DIR : $SCRIPT_DIR"
 #echo "Writing to : $LOGFILE"
 #echo "PI_ID is $PI_ID"
 #echo "Timestamp is $TIMESTAMP"
-echo "Top Process is : $TOP_PROCESS"
+
+#echo "Top Process is : $TOP_PROCESS"
+#echo "Config File is : $CONFIG_FILE"
 
 # Get Temperature
 TEMP=$(vcgencmd measure_temp | grep -o  '[0-9]*\.[0-9]*')
